87b17af71a3a74af2a108f5712c540abf53be9ef
Vineet Reddy <hotshot2797@gmail.com>
2018-03-05 18:49:41 +0530

    Changes

--- main.js
+++ main.js
@@ -2,7 +2,13 @@ mapboxgl.accessToken = 'pk.eyJ1IjoidmluZWV0cmVkZHkiLCJhIjoiY2pkeWcxOXl6MTMxczJ3c
 
 
 var chapters = {
-  'part-1' : {
+    'part-0' : {
+        center : [78.9629,20.5937],
+        zoom : 4,
+        bearing : 0,
+        pitch : 0,
+    },
+'part-1' : {
 center : [77.123,28.653],
 zoom : 10,
 bearing : 0,
@@ -11,8 +17,8 @@ pitch : 1,
 'part-2' : {
 center : [72.83333,18.96667],
 zoom : 11,
-bearing : -50,
-pitch : 55,
+bearing : 0,
+pitch : 50,
 },
 'part-3' : {
 center : [77.59796,12.96991],
@@ -28,40 +34,46 @@ pitch : 1,
 },
 'part-5' : {
 center : [88.33778,22.54111],
-zoom : 4,
+zoom : 11,
 bearing : 0,
 pitch : 1,
 },
 'part-6' : {
 center : [78.46667,17.36667],
-zoom : 4,
+zoom : 11,
 bearing : 0,
 pitch : 1,
 },
 'part-7' : {
 center : [76.266935,9.901199],
-zoom : 4,
+zoom : 11,
 bearing : 0,
 pitch : 1,
 },
 'part-8' : {
 center : [72.58,23.03],
-zoom : 4,
+zoom : 11,
 bearing : 0,
 pitch : 1,
 },
 'part-9' : {
 center : [74.030543,15.29299],
-zoom : 4,
+zoom : 11,
 bearing : 0,
 pitch : 1,
 },
 'part-10' : {
 center : [73.84778,18.52361],
-zoom : 4,
+zoom : 11,
 bearing : 0,
 pitch : 1,
-}
+},
+'part-11' : {
+    center : [78.9629,20.5937],
+    zoom : 4,
+    bearing : 0,
+    pitch : 0,
+},
 };
 
 var map;
